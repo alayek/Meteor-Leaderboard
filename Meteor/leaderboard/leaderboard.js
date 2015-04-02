2d2ac58f7ef10e2825b29be1f80bb5bad23873a9
Arijit Layek <notmyemail@example.com>
2015-04-02 19:02:20 +0000

    Remove autopublish

--- Meteor/leaderboard/leaderboard.js
+++ Meteor/leaderboard/leaderboard.js
@@ -32,67 +32,130 @@ PlayerList.insert({
     score: 0
 });
 */
-if (Meteor.isClient) {
-  // helpers for templates
+
+// Code that only runs on the client 
+if(Meteor.isClient){
+
+  // "Catch" the selection of data from the Meteor.publish function
+  Meteor.subscribe('thePlayers');
+
+  // Helper functions 
   Template.leaderboard.helpers({
     'player': function(){
-      // get user specific leaderboard
-      var currentUser = Meteor.userId();
-       return PlayerList.find({createdBy: currentUser}, {sort: {score: -1}}); 
+
+      // Get the ID of the current user
+      var currentUserId = Meteor.userId();
+
+      // Retrieve data that belongs to the current user
+      return PlayerList.find({createdBy: currentUserId},
+                              {sort: {score: -1, name: 1}});
+
     },
     
-    'selectedClass' : function(){
-      var playerId = this._id;
+    'showSelectedPlayer': function(){
+
+      // Get the ID of the player that's been clicked
       var selectedPlayer = Session.get('selectedPlayer');
-      if (selectedPlayer === playerId) {
-        return "selected";
-      }
+
+      // Retrieve a single document from the collection
+      return PlayerList.findOne(selectedPlayer)
+
     },
-    
-    'showSelectedPlayer' : function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      return PlayerList.findOne(selectedPlayer);
-    }
-  });
-  
-  Template.leaderboard.events({
-    
-    'click .player' : function(){
+    'selectedClass': function(){
+
+        // Get the ID of the player being iterated through
         var playerId = this._id;
-        Session.set('selectedPlayer', playerId);
+
+        // Get the ID of the player that's been clicked
         var selectedPlayer = Session.get('selectedPlayer');
-    },
-    
-    'click .increment' : function(){
-      var selectedPlayer = Session.get('selectedPlayer');
-      PlayerList.update(selectedPlayer, {$inc: {score: 5}});
-    },
-    
-    'click .decrement' : function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      PlayerList.update(selectedPlayer, {$inc: {score: -5}});
-    },
-    
-    'click .remove' : function(){
-      var selectedPlayer = Session.get('selectedPlayer');
-      PlayerList.remove(selectedPlayer);
+
+         // Do these IDs match?
+        if(playerId == selectedPlayer){
+
+            // Return a CSS class
+            return "selected"
+
+        }
+
     }
   });
-  
+
+  // Events 
+  Template.leaderboard.events({
+      'click .player': function(){
+
+          // Retrieve the unique ID of the player that's been clicked
+          var playerId = this._id;
+
+          // Create a session to store the unique ID of the clicked player
+          Session.set('selectedPlayer', playerId);
+
+      },
+      'click .increment': function(){
+
+        // Get the ID of the player that's been clicked
+        var selectedPlayer = Session.get('selectedPlayer');
+
+        // Update a document and increment the score field by 5
+        PlayerList.update(selectedPlayer, {$inc: {score: 5} });
+
+      },
+      'click .decrement': function(){
+
+        // Get the ID of the player that's been clicked
+        var selectedPlayer = Session.get('selectedPlayer');
+
+        // Update a document and decrement the score field by 5
+        PlayerList.update(selectedPlayer, {$inc: {score: -5} });
+
+      },
+      'click .remove': function(){
+
+        // Get the ID of the player that's been clicked
+        var selectedPlayer = Session.get('selectedPlayer');
+
+        // Remove a document from the collection
+        PlayerList.remove(selectedPlayer);
+
+      }
+  });
+
   Template.addPlayerForm.events({
-    'submit form' : function(event) {
-      event.preventDefault();
-      var playerName = event.target.playerName.value;
-      // make this user specific
-      var currentUser = Meteor.userId();
-      PlayerList.insert({
-        name: playerName,
-        score: 0,
-        createdBy: currentUser
-      });
+    'submit form': function(event){
+
+        // Prevent the browser from applying default behaviour to the form
+        event.preventDefault();
+
+        // Get the value from the "playerName" text field
+        playerNameVar = event.target.playerName.value;
+
+        // Get the ID of the current user
+        var currentUserId = Meteor.userId();
+
+        // Insert the new player into the collection
+        PlayerList.insert({
+          name: playerNameVar,
+          score: 0,
+          createdBy: currentUserId
+        });
+
     }
   });
+
 }
 
+// Code that only runs on the server 
+if(Meteor.isServer){
 
+  // Transmit a selection of data into the ether
+  Meteor.publish('thePlayers', function(){
 
+    // Get the ID of the current user
+    var currentUserId = this.userId;
+
+    // Return players "owned" by the current user
+    return PlayerList.find({createdBy: currentUserId})
+
+  });
+
+}
